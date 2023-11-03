535d898e44c6c2a2a1e362458b02da282d8b88ad
tilisma <tilisma123.pahari@gmail.com>
2023-11-03 13:37:54 +0545

    fakestoreapi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [categories, setCategories] = useState([]);
+  const [products, setproducts] = useState([]); 
+  const [jewelery,setjewelery] = useState([]); 
+  const [singleproduct, setSingleproduct] = useState([]);
+
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => setproducts(json));
+  fetch("https://fakestoreapi.com/products/categories")
+    .then((res) => res.json())
+    .then((json) => setCategories(json)); 
+  fetch('https://fakestoreapi.com/products/category/jewelery')
+            .then(res=>res.json())
+            .then(json=>setjewelery(json)) 
+  fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>setSingleproduct(json))
+  return ( 
+    <>
+    <div>
+      <ul>
+        {categories.map((category) => (
+          <li>{category}</li>
+        ))}
+      </ul>
+
+      <ul>
+        {products.map((product) => ( 
+    <div style={{margin:"50px"}}>
+          <li>Title:{product.title}</li> 
+          <li>Price:{product.price}</li> 
+          <li>Id:{product.id}</li>  
+          <li>Description:{product.description}</li> 
+          </div>
+        ))}   
+      </ul> 
+
+      <ul>
+        {jewelery.map((jewel)=>( 
+          <div style={{margin:"50px"}}>
+            <li>Id:{jewel.id}</li>
+            <li>Title:{jewel.title}</li> 
+            <li>Description:{jewel.description}</li> 
+            <li>Price:{jewel.price}</li> 
+            <img src={jewel.image} style={{height:"100px"}}/> 
+          </div> 
+        ))} 
+      </ul> 
+      <ul>
+          <div style={{margin:"50px"}}>
+            <li>Id:{singleproduct.id}</li>
+            <li>Title:{singleproduct.title}</li> 
+            <li>Description:{singleproduct.description}</li> 
+            <li>Price:{singleproduct.price}</li> 
+            <img src={singleproduct.image} style={{height:"100px"}}/> 
+          </div> 
+         
+      </ul> 
+    
+    </div> 
+    </> 
   );
 }
 
